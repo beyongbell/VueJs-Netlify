7b659fbf31090fd290e925308b78ce9a435e7c6a
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-09-30 17:23:38 +0700

    asdasd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }} Tinnakorn</h1>
+    <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
+      For a guide and recipes on how to configure / customize this project,
+      asdasdasdasd<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
